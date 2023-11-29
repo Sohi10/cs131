4247f817e3d1b74ff463bf986fe6443c666e9490
Sohi10 <jassasohi771@gmail.com>
2023-11-29 15:40:01 -0500

    5 files

--- a5/script.sh
+++ a5/script.sh
@@ -1,6 +1,6 @@
 #!bin/bash/
 
 awk -f p.awk 2020coviddata.csv > total_deaths.csv
-sort -t, -k2nr total_deaths.csv > top_three.dat
+sort -t, -k2nr total_deaths.csv | head -3 > top_three.dat
 
 gnuplot plot.gp
